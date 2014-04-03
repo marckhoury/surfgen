6e1e8af4af901092fe019d800fc0969fbdba891c
Marc Khoury <marc.khry@gmail.com>
2014-04-03 00:27:39 -0700

    Removed cut functions from gengrid.

--- gengrid.h
+++ gengrid.h
@@ -1,11 +1,8 @@
 #ifndef GENGRID_H
 #define GENGRID_H
 
-#include <cmath>
 #include "grid.h"
-
-enum SurfaceType {JACK, WIFFLE_CUBE, TORUS, SPHERE, MOBIUS_STRIP, KLEIN_BOTTLE, HYPERBOLOID_ONE_SHEET, CAYLEY, BOHEMIAN_DOME, CHUBS, QUADRIFOLIA, DEVIL, QUARTIC_CYLINDER, GUMDROP_TORUS, BARTH_SEXTIC, BOHEMIAN_STAR, NO_SURFACE};
+#include "cutfunc.h"
 
 void generate(Grid& grid, SurfaceType surface_type);
 #endif
-
